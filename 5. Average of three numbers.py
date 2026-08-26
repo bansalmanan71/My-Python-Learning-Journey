c5c99122c68e39b6c09e0bc9b8f2826f6c84e079
Manan <bansalmanan71@gmail.com>
2026-08-26 21:12:46 +0530

    Add files via upload

---
+++ 5. Average of three numbers.py
@@ -0,0 +1,8 @@
+print("This is a program to find Average of 3 Numbers")
+num1=float(input("Enter the num1 : "))
+num2=float(input("Enter the num2 : "))
+num3=float(input("Enter the num3 : "))
+
+Average = (num1+num2+num3) / 3
+
+print("The average of the three numvers is : ", Average)
