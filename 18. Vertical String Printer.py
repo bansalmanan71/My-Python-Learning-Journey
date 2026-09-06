1ce7828fa5ff704dd388897a01488b1e3f2dc12f
Manan <bansalmanan71@gmail.com>
2026-09-06 19:15:17 +0530

    Update 18. Vertical String Printer.py
    Remove unnecessary string slicing

--- 18. Vertical String Printer.py
+++ 18. Vertical String Printer.py
@@ -1,4 +1,4 @@
 print("Welcome to Vertical String Printer")
 print("In this program, whatever you type, will be converted vertically into characters")
 x=input("Enter: ")
-print(*x[0:], sep="\n")
+print(*x, sep="\n")
