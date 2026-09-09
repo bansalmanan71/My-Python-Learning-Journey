9219fcdcc7ca8a7f7fa8558067be9ca2511c6e4d
bansalmanan71 <bansalmanan71@gmail.com>
2026-09-09 18:19:09 +0530

    Update "First Character Vowel Checker.py"

--- 19. First Character Vowel Checker.py
+++ 19. First Character Vowel Checker.py
@@ -1,6 +1,8 @@
 print("This program checks if the first character of your input is a vowel or not")
 a=input("Enter: ")
-if a[0]== 'a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u' or a[0]=='A' or a[0]=='E' or a[0]=='I' or a[0]=='O' or a[0]=='U':
+# if a[0]== 'a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u' or a[0]=='A' or a[0]=='E' or a[0]=='I' or a[0]=='O' or a[0]=='U':
+# Instead we can use in
+if a[0] in "aeiouAEIOU":
     print(f"The first character '{a[0]}' is a vowel")
 else:
     print(f"The first character '{a[0]}' is not a vowel")
